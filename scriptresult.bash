98c0b2f30c21030635ddb22de8481b03b5a51dd3
ThePreatorian9 <122523486+ThePreatorian9@users.noreply.github.com>
2023-08-01 18:03:02 +0200

    Add file for the demo Numspot
    c'est pour la demo avec un exemple rapid eavec du python

---
+++ scriptresult.bash
@@ -0,0 +1,16 @@
+#!bin/bash
+
+docker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.18.3 image   $1 > resultatanalyse.txt && awk "/^Total/{print}" resultatanalyse.txt
+save=$(awk "/^Total/{print}" resultatanalyse.txt  | head -n1 | awk '{print $2;}')
+echo $save
+if [ $save -ne 0 ]; then
+        echo "In If erreur détecté "
+        exit 1
+else
+         
+         echo " Tout va bien"
+
+
+fi
+
+
